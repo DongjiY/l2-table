5b068aac48a57d6c81d1cbfbf4e232d6cfe8c24b
Dongji Yang <dongji.yang@outlook.com>
2026-01-10 13:55:43 -0800

    add scrollbar support

--- src/table/table.ts
+++ src/table/table.ts
@@ -5,11 +5,18 @@ import { RowData, SourceData, TableConfig } from "./table-config";
 import { TableBody } from "./body";
 import { TableHeader } from "./header";
 import { CanvasRenderer } from "../canvas/canvas-renderer";
+import { HorizontalScrollbar } from "../scroll/horizontal-scrollbar";
+import { VerticalScrollbar } from "../scroll/vertical-scrollbar";
 
 export class Table<TRow extends RowData> {
   private container: HTMLDivElement;
+  private verticalWrapper: HTMLDivElement;
+  private horizontalWrapper: HTMLDivElement;
+
   private header: TableHeader<TRow>;
   private body: TableBody<TRow>;
+  private xScrollBar: HorizontalScrollbar;
+  private yScrollBar: VerticalScrollbar;
   private canvasRenderer: CanvasRenderer;
   private readonly camera: Camera;
 
@@ -19,8 +26,16 @@ export class Table<TRow extends RowData> {
     tableConfig: TableConfig<TRow>
   ) {
     this.container = container;
-    this.container.style.display = "flex";
-    this.container.style.flexDirection = "column";
+
+    this.horizontalWrapper = document.createElement("div");
+    this.horizontalWrapper.style.display = "flex";
+    this.container.appendChild(this.horizontalWrapper);
+
+    this.verticalWrapper = document.createElement("div");
+    this.verticalWrapper.style.display = "flex";
+    this.verticalWrapper.style.flexDirection = "column";
+    this.horizontalWrapper.appendChild(this.verticalWrapper);
+
     this.camera = new Camera({
       viewportHeight: 500,
       viewportWidth: 600,
@@ -45,9 +60,30 @@ export class Table<TRow extends RowData> {
       this.camera
     );
 
-    this.canvasRenderer = new CanvasRenderer([this.header, this.body]);
+    this.xScrollBar = new HorizontalScrollbar(
+      {
+        w: 600,
+      },
+      this.camera
+    );
 
-    this.header.attach(this.container);
-    this.body.attach(this.container);
+    this.yScrollBar = new VerticalScrollbar(
+      {
+        h: 540,
+      },
+      this.camera
+    );
+
+    this.canvasRenderer = new CanvasRenderer([
+      this.header,
+      this.body,
+      this.xScrollBar,
+      this.yScrollBar,
+    ]);
+
+    this.header.attach(this.verticalWrapper);
+    this.body.attach(this.verticalWrapper);
+    this.xScrollBar.attach(this.verticalWrapper);
+    this.yScrollBar.attach(this.horizontalWrapper);
   }
 }
