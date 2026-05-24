6da6c4136fbc8fef722a05432e284879cfac502e
Dongji Yang <69827294+DongjiY@users.noreply.github.com>
2026-05-23 17:18:25 -0700

    add layouter class to compute static content widths (#91)
    This PR implements the layouter class that allows a layout pass to record the widths of all static content being drawn
    
    - layouter.layout records and continously sums static content width
    - layouter.start begins a new layout pass
    - layouter.end clears the state and prepares for a new layouter pass

--- packages/core/src/utils/draw-canvas.ts
+++ packages/core/src/utils/draw-canvas.ts
@@ -4,15 +4,18 @@ import { Drawable } from "./drawable";
 import { Canvas } from "./canvas";
 import { Dimensions } from "./dimensions";
 import { Painter } from "./painter";
+import { Layouter } from "./layouter";
 
 export abstract class DrawCanvas extends Canvas implements Closeable, Drawable {
   private readonly painter: Painter;
   private drawQueue$: ReplaySubject<void>;
+  private readonly layouter: Layouter;
 
   constructor(dimensions: Dimensions) {
     super(dimensions);
 
-    this.painter = new Painter(this.canvas.getContext("2d")!);
+    this.layouter = new Layouter();
+    this.painter = new Painter(this.canvas.getContext("2d")!, this.layouter);
     this.drawQueue$ = new ReplaySubject(1);
   }
 
@@ -38,6 +41,8 @@ export abstract class DrawCanvas extends Canvas implements Closeable, Drawable {
   public abstract draw(painter: Painter): void;
 
   public _drawImpl(): void {
+    this.layouter.start();
+
     this.painter.dangerouslyDrawWithCanvasCtx((ctx) => {
       ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -45,6 +50,8 @@ export abstract class DrawCanvas extends Canvas implements Closeable, Drawable {
       this.draw(this.painter);
       ctx.restore();
     });
+
+    this.layouter.stop();
   }
 
   public close(): void {
